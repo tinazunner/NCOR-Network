abc72c6c4234cb9b96f2a7b6c5a20aa9051a7799
jravenel <jeremy.ravenel@cashstory.com>
2025-04-13 21:16:39 -0400

    fix: ontology cards

--- website/src/components/HomepageFeatures.jsx
+++ website/src/components/HomepageFeatures.jsx
@@ -1,98 +1,83 @@
 import React from 'react';
 import styles from './HomepageFeatures.module.css';
 
-// Import all images at the top
-const BFOImage = require('@site/static/img/cards/BasicFormalOntology_card.png').default;
-const GeospatialImage = require('@site/static/img/cards/GeospatialOntology_card.png').default;
-const InformationEntityImage = require('@site/static/img/cards/InformationEntityOntology_card.png').default;
-const EventImage = require('@site/static/img/cards/EventOntology_card.png').default;
-const TimeImage = require('@site/static/img/cards/TimeOntology_card.png').default;
-const AgentImage = require('@site/static/img/cards/AgentOntology_card.png').default;
-const QualityImage = require('@site/static/img/cards/QualityOntology_card.png').default;
-// Add imports for the remaining ontologies (or use the same images temporarily)
-const ActionImage = TimeImage; // temporary placeholder
-const ArtifactImage = GeospatialImage; // temporary placeholder
-const InformationBearerImage = InformationEntityImage; // temporary placeholder 
-const PhysicalEntityImage = QualityImage; // temporary placeholder
-const RelationImage = AgentImage; // temporary placeholder
-
 const FeatureList = [
   {
     title: 'Basic Formal Ontology (BFO)',
-    image: BFOImage || QualityImage,
+    image: require('@site/static/img/cards/BasicFormalOntology_card.png').default,
     description: 'A top-level ontology that provides a framework for building domain ontologies. BFO serves as the foundation for all Common Core Ontologies and represents the most basic categories of reality.',
+    githubUrl: 'https://raw.githubusercontent.com/BFO-ontology/BFO-2020/refs/heads/master/src/owl/bfo-core.ttl'
   },
   {
-    title: 'Geospatial Ontology',
-    image: GeospatialImage,
-    description: 'Designed to represent sites, spatial regions, and other entities, especially those that are located near the surface of Earth.',
-  },
-  {
-    title: 'Information Entity Ontology',
-    image: InformationEntityImage,
-    description: 'Designed to represent generic types of information as well as the relationships between information and other entities.',
-  },
-  {
-    title: 'Event Ontology',
-    image: EventImage,
-    description: 'Designed to represent processual entities, especially those performed by agents, that occur within multiple domains.',
-  },
-  {
-    title: 'Time Ontology',
-    image: TimeImage,
-    description: 'Designed to represent temporal regions and the relations that hold between them.',
-  },
-  {
-    title: 'Agent Ontology',
-    image: AgentImage,
+    title: 'Agent Ontology (CCO)',
+    image: require('@site/static/img/cards/AgentOntology_card.png').default,
     description: 'Designed to represent agents, especially persons and organizations, and their roles.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/AgentOntology.ttl'
   },
   {
-    title: 'Quality Ontology',
-    image: QualityImage,
+    title: 'Artifact Ontology (CCO)',
+    image: require('@site/static/img/cards/GeospatialOntology_card.png').default, // Using placeholder image
+    description: 'Designed to represent artifacts that are common to multiple domains along with their models, specifications, and functions',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/ArtifactOntology.ttl'
+  },
+  {
+    title: 'Currency Unit Ontology (CCO)',
+    image: require('@site/static/img/cards/CurrencyUnitOntology_card.png').default, // Using placeholder image
+    description: 'Designed to represent currencies that are issued and used by countries.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/CurrencyUnitOntology.ttl'
+  },
+  {
+    title: 'Event Ontology (CCO)',
+    image: require('@site/static/img/cards/EventOntology_card.png').default,
+    description: 'Designed to represent processual entities, especially those performed by agents, that occur within multiple domains.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/EventOntology.ttl'
+  },
+  {
+    title: 'Extended Relation Ontology (CCO)',
+    image: require('@site/static/img/cards/ExtendedRelationOntology_card.png').default, // Using placeholder image
+    description: 'Designed to represent many of the relations (i.e. object properties) that hold between entities at the level of the mid-level Common Core Ontologies.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/ExtendedRelationOntology.ttl'
+  },
+  {
+    title: 'Facility Ontology (CCO)',
+    image: require('@site/static/img/cards/FacilityOntology_card.png').default, // Using placeholder image
+    description: 'Designed to represent buildings and campuses that are designed to serve some specific purpose and which are common to multiple domains.', 
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/FacilityOntology.ttl'
+  },
+  {
+    title: 'Geospatial Ontology (CCO)',
+    image: require('@site/static/img/cards/GeospatialOntology_card.png').default,
+    description: 'Designed to represent sites, spatial regions, and other entities, especially those that are located near the surface of Earth, as well as the relations that hold between them.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/GeospatialOntology.ttl'
+  },
+  {
+    title: 'Information Entity Ontology (CCO)',
+    image: require('@site/static/img/cards/InformationEntityOntology_card.png').default,
+    description: 'Designed to represent generic types of information as well as the relationships between information and other entities.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/InformationEntityOntology.ttl'
+  },
+  {
+    title: 'Quality Ontology (CCO)',
+    image: require('@site/static/img/cards/QualityOntology_card.png').default,
     description: 'Designed to represent a range of attributes of entities especially qualities, realizable entities, and process profiles.',
-  },
-  // Add the remaining ontologies
-  {
-    title: 'Action Ontology',
-    image: ActionImage,
-    description: 'Designed to represent intentional actions performed by agents to achieve specific goals or objectives.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/QualityOntology.ttl'
   },
   {
-    title: 'Artifact Ontology',
-    image: ArtifactImage,
-    description: 'Designed to represent human-made objects created to serve specific functions or purposes.',
+    title: 'Time Ontology (CCO)',
+    image: require('@site/static/img/cards/TimeOntology_card.png').default,
+    description: 'Designed to represent temporal regions and the relations that hold between them..',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/develop/src/cco-modules/TimeOntology.ttl'
   },
   {
-    title: 'Information Bearer Ontology',
-    image: InformationBearerImage,
-    description: 'Designed to represent physical entities that carry or convey information content.',
-  },
-  {
-    title: 'Physical Entity Ontology',
-    image: PhysicalEntityImage,
-    description: 'Designed to represent material entities that exist in physical space and time.',
-  },
-  {
-    title: 'Relation Ontology',
-    image: RelationImage,
-    description: 'Designed to represent relationships and connections between different types of entities.',
+    title: 'Unit Of Measure Ontology (CCO)',
+    image: require('@site/static/img/cards/UnitsOfMeasureOntology_card.png').default, // Using placeholder image
+    description: 'Designed to represent standard measurement units that are used when measuring various attributes of entities.',
+    githubUrl: 'https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/refs/heads/develop/src/cco-modules/UnitsOfMeasureOntology.ttl'
   },
 ];
 
-function FeatureCard({title, image, description}) {
+function FeatureCard({title, image, description, githubUrl}) {
   const cardClassName = title.toLowerCase().replace(/\s+/g, '-') + '-ontology';
-  
-  // Create the GitHub URL based on the ontology name
-  let githubUrl;
-  
-  // Special case for BFO
-  if (title === 'Basic Formal Ontology (BFO)') {
-    githubUrl = 'https://raw.githubusercontent.com/BFO-ontology/BFO-2020/refs/heads/master/src/owl/bfo-core.ttl';
-  } else {
-    const ontologyFileName = title.replace(/\s+/g, '') + '.ttl';
-    githubUrl = `https://github.com/CommonCoreOntology/CommonCoreOntologies/blob/develop/src/cco-modules/${ontologyFileName}`;
-  }
   
   // Use inline style with the imported image
   const headerStyle = {
